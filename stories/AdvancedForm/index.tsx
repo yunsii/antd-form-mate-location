6831c3caf2a2b965d4051d37ce570fa2ea03d861
zpr1g <yuns.xie@qq.com>
2020-04-07 23:04:12 +0800

    refactor: by antd-form-mate

--- stories/AdvancedForm/index.tsx
+++ stories/AdvancedForm/index.tsx
@@ -1,69 +1,57 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Form, Button } from 'antd';
-import { createFormItems, IntlProvider as FormIntlProvider, enUSIntl as formEnUSIntl } from 'antd-form-mate';
-import { ItemConfig } from 'antd-form-mate/dist/lib/props';
+import FormMate, { IntlProvider as FormIntlProvider, enUSIntl as formEnUSIntl } from 'antd-form-mate';
 import { IntlProvider, enUSIntl } from '../../src';
 
 const initialValues = {
-  location: { position: { longitude: 114.104624, latitude: 22.554863 }, formattedAddress: "广东省深圳市罗湖区桂园街道红岭2118号大院建设集团大厦" },
+  location: {
+    position: { longitude: 114.104624, latitude: 22.554863 },
+    formattedAddress: '广东省深圳市罗湖区桂园街道红岭2118号大院建设集团大厦',
+  },
 };
 
 class AdvancedForm extends React.Component {
-  setFormItemsConfig = (detail: any = {}, mode?: string): ItemConfig[] => {
-    return [
-      {
-        type: 'string',
-        name: 'string',
-        formItemProps: {
-          label: '字符串',
-        },
-      },
-      {
-        type: 'location' as any,
-        name: 'location',
-        formItemProps: {
-          label: '地址',
-        },
-      },
-    ];
-  }
-
   handleFinish = (values) => {
     console.log('Received values of form: ', values);
-  }
+  };
 
   handleFinishFailed = (errors) => {
     console.log('Errors:', errors);
-  }
+  };
 
   render() {
+    const newProps = {
+      type: 'location',
+    } as any;
+
     return (
       <FormIntlProvider value={formEnUSIntl}>
         <IntlProvider value={enUSIntl}>
-          <Form
+          <FormMate
             style={{ marginTop: 20 }}
             onFinish={this.handleFinish}
             onFinishFailed={this.handleFinishFailed}
             initialValues={initialValues}
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 12,
+            }}
           >
-            {createFormItems(this.setFormItemsConfig({}))}
+            <FormMate.Item type="string" name="string" label="字符串" />
+            <FormMate.Item {...newProps} name="location" label="地址" />
             <Form.Item wrapperCol={{ span: 12, offset: 8 }}>
-              <Button
-                type="primary"
-                htmlType="submit"
-                onClick={action('click submit')}
-              >
+              <Button type="primary" htmlType="submit" onClick={action('click submit')}>
                 提交
-          </Button>
+              </Button>
             </Form.Item>
-          </Form>
+          </FormMate>
         </IntlProvider>
       </FormIntlProvider>
-    )
+    );
   }
 }
 
 export default AdvancedForm;
-
-
